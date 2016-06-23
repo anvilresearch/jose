486b2b1a2f94cca2ce3a0e11a172c002069e95d6
Christian Smith <smith@anvil.io>
2016-06-23 14:19:26 -0700

    feat(RSAKeyPair): initial class definition

---
+++ src/keys/RSAKeyPair.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/**
+ * Dependencies
+ */
+const KeyPair = require('./KeyPair')
+
+/**
+ * RSAKeyPair
+ */
+class RSAKeyPair extends KeyPair {}
+
+/**
+ * Export
+ */
+module.exports = RSAKeyPair

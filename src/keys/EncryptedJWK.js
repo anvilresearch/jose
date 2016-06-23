9722e67c4ca76515f9e92a692585c83e1af73780
Christian Smith <smith@anvil.io>
2016-06-23 16:10:52 -0700

    feat(EncryptedJWK): placeholder methods for serializing instance as JWE

--- src/keys/EncryptedJWK.js
+++ src/keys/EncryptedJWK.js
@@ -3,11 +3,26 @@
 /**
  * Dependencies
  */
+const JWK = require('./JWK')
 
 /**
  * EncryptedJWK
+ *
+ * @class
+ * Adds methods suitable for serializing and deserializing a private key JWK.
  */
-class EncryptedJWK {}
+class EncryptedJWK extends JWK {
+
+  /**
+   * Serialize
+   */
+  toJWE () {}
+
+  /**
+   * Deserialize
+   */
+  static fromJWE () {}
+}
 
 /**
  * Export

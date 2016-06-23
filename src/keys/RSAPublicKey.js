72b6a37bf54c3d0b1f7d84fa001906e8113e3ab9
Christian Smith <smith@anvil.io>
2016-06-23 14:20:36 -0700

    feat(RSAPublicKey): initial class definition

---
+++ src/keys/RSAPublicKey.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/**
+ * Dependencies
+ */
+const JWK = require('./JWK')
+
+/**
+ * RSAPublicKey
+ */
+class RSAPublicKey extends JWK {}
+
+/**
+ * Export
+ */
+module.exports = RSAPublicKey

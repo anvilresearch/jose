2c2bdcfc2078d02a8fd84678f7ffa61cf9115d39
Christian Smith <smith@anvil.io>
2016-06-23 16:14:02 -0700

    feat(KeyPair): initial schema definition and abstract generate method

--- src/keys/KeyPair.js
+++ src/keys/KeyPair.js
@@ -7,7 +7,27 @@
 /**
  * KeyPair
  */
-class KeyPair {}
+class KeyPair {
+
+  /**
+   * Schema
+   */
+  static get schema () {
+    return {
+      properties: {
+        pub: { type: 'object' },
+        prv: { type: 'object' }
+      }
+    }
+  }
+
+  /**
+   * Generate
+   */
+  static generate () {
+    throw new Error('KeyPair generate method must be implemented by extending class')
+  }
+}
 
 /**
  * Export

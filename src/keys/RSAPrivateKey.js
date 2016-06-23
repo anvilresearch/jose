42bac680c6dcbea1a996753b3f3337fe3ef24ff2
Christian Smith <smith@anvil.io>
2016-06-23 16:16:30 -0700

    feat(RSAPrivateKey): initial schema definition

--- src/keys/RSAPrivateKey.js
+++ src/keys/RSAPrivateKey.js
@@ -8,7 +8,25 @@ const EncryptedJWK = require('./EncryptedJWK')
 /**
  * RSAPrivateKey
  */
-class RSAPrivateKey {}
+class RSAPrivateKey extends EncryptedJWK {
+
+  /**
+   * Schema
+   */
+  static get schema () {
+    return {
+      properties: {
+        d: { type: 'string' },
+        p: { type: 'string' },
+        q: { type: 'string' },
+        dp: { type: 'string' },
+        dq: { type: 'string' },
+        qi: { type: 'string' },
+        oth: { type: 'string' },
+      }
+    }
+  }
+}
 
 /**
  * Export

0ce38b3aa30cca3e1e8c4c64ffcfb46344f8640b
Christian Smith <smith@anvil.io>
2016-06-24 15:56:42 -0700

    feat(ECKeyPair): generate PEM-encoded Elliptic Curve key pair

--- src/keys/ECKeyPair.js
+++ src/keys/ECKeyPair.js
@@ -2,11 +2,17 @@
 
 /**
  * Dependencies
+ * @ignore
  */
+const {spawn} = require('child_process')
 const KeyPair = require('./KeyPair')
 
 /**
  * ECKeyPair
+ *
+ * @class
+ * ECKeyPair represents an Elliptic Curve cryptographic keypair in memory.
+ * It maintains both PEM and JWK representations.
  */
 class ECKeyPair extends KeyPair {
 
@@ -14,8 +20,34 @@ class ECKeyPair extends KeyPair {
    * Generate
    */
   static generate () {
-    return Promise.resolve('TBD')
+    return new Promise((resolve, reject) => {
+      let keypair = {}
+      let ecparam = spawn('openssl', ['ecparam', '-name', 'secp256k1', '-genkey'])
+      let ec = spawn('openssl', ['ec', '-pubout'])
+
+      ecparam.stdout.on('data', (data) => {
+        keypair.prv = data.toString('ascii')
+        ec.stdin.write(data)
+      })
+
+      ec.stdout.on('data', (data) => {
+        keypair.pub = data.toString('ascii')
+      })
+
+      ec.on('close', (code) => {
+        resolve(new ECKeyPair(keypair))
+      })
+    })
   }
+
+  /**
+   * Constructor
+   */
+  constructor (data) {
+    super(data)
+    Object.assign(this, data)
+  }
+
 }
 
 /**

8ffa7620e621df675446505c8977234249914f4e
Christian Smith <smith@anvil.io>
2016-06-24 14:22:29 -0700

    feat(RSAKeyPair): generate PEM-encoded RSA keypair

--- src/keys/RSAKeyPair.js
+++ src/keys/RSAKeyPair.js
@@ -3,6 +3,7 @@
 /**
  * Dependencies
  */
+const {spawn} = require('child_process')
 const KeyPair = require('./KeyPair')
 
 /**
@@ -13,8 +14,38 @@ class RSAKeyPair extends KeyPair {
   /**
    * Generate
    */
-  static generate () {
-    return Promise.resolve('TBD')
+  static generate (bitlength = 4096) {
+    return new Promise((resolve, reject) => {
+      let keypair = {}
+      let genrsa = spawn('openssl', ['genrsa', bitlength])
+      let rsa = spawn('openssl', ['rsa', '-pubout'])
+
+      // store private key pem on the keypair
+      // and pipe stdout to the public key process
+      genrsa.stdout.on('data', (data) => {
+        keypair.prv = data.toString('ascii')
+        rsa.stdin.write(data)
+      })
+
+      // store public key pem on the keypair
+      rsa.stdout.on('data', (data) => {
+        keypair.pub = data.toString('ascii')
+      })
+
+      // cast the keypair to RSAKeyPair
+      // and resolve the promise
+      rsa.on('close', (code) => {
+        resolve(new RSAKeyPair(keypair))
+      })
+    })
+  }
+
+  /**
+   * Constructor
+   */
+  constructor (data) {
+    super(data)
+    Object.assign(this, data)
   }
 }
 

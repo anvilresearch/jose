313a7ffb3691c8b5f55d5aa67ffd8559d3c28d3a
Christian Smith <smith@anvil.io>
2016-06-23 22:28:56 -0700

    test(RSAPrivateKey) schema definition tests

--- src/keys/RSAPrivateKey.js
+++ src/keys/RSAPrivateKey.js
@@ -16,13 +16,85 @@ class RSAPrivateKey extends EncryptedJWK {
   static get schema () {
     return {
       properties: {
-        d: { type: 'string' },
-        p: { type: 'string' },
-        q: { type: 'string' },
-        dp: { type: 'string' },
-        dq: { type: 'string' },
-        qi: { type: 'string' },
-        oth: { type: 'string' },
+
+        n: {
+          type: 'string',
+          format: 'Base64urlUInt',
+          required: true
+        },
+
+        e: {
+          type: 'string',
+          format: 'Base64urlUInt',
+          required: true
+        },
+
+        d: {
+          type: 'string',
+          format: 'Base64urlUInt',
+          required: true
+        },
+
+        // TODO
+        // - if the produce includes any of the other private key
+        //   parameters, then all of the others MUST be present,
+        //   with the exception of "oth", which MUST only be present
+        //   when more than two prime factors were used.
+        p: {
+          type: 'string',
+          format: 'Base64urlUInt'
+        },
+
+        q: {
+          type: 'string',
+          format: 'Base64urlUInt'
+        },
+
+        dp: {
+          type: 'string',
+          format: 'Base64urlUInt'
+        },
+
+        dq: {
+          type: 'string',
+          format: 'Base64urlUInt'
+        },
+
+        qi: {
+          type: 'string',
+          format: 'Base64urlUInt'
+        },
+
+        // TODO
+        // - When only two primes have been used, (the normal case), this
+        //   parameter MUST be omitted
+        // - When three or more primes have been used, the number of array
+        //   elements MUST be the number of primes used minus two.
+        oth: {
+          type: 'array',
+          items: [
+            {
+              type: 'object',
+              properties: {
+                r: {
+                  type: 'string',
+                  format: 'Base64urlUInt',
+                  required: true
+                },
+                d: {
+                  type: 'string',
+                  format: 'Base64urlUInt',
+                  required: true
+                },
+                t: {
+                  type: 'string',
+                  format: 'Base64urlUInt',
+                  required: true
+                }
+              }
+            }
+          ]
+        }
       }
     }
   }

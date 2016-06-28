979498737b9d54e11151b157c4406a1dabab6694
Christian Smith <smith@anvil.io>
2016-06-28 12:58:38 -0700

    feat(JWA): initial RSASSA-PKCS1-v1_5 class definition

---
+++ src/tokens/algs/RSASSA-PKCS1-v1_5.js
@@ -0,0 +1,65 @@
+'use strict'
+
+/**
+ * Dependencies
+ * @ignore
+ */
+const base64url = require('base64url')
+const crypto = require('crypto')
+
+/**
+ * RSASSA-PKCS1-v1_5
+ */
+class RSASSA_PKCS1_v1_5 {
+
+  /**
+   * Constructor
+   *
+   * @param {string} bitlength
+   */
+  constructor (bitlength) {
+    this.bitlength = bitlength
+  }
+
+  /**
+   * Sign
+   *
+   * @description
+   * Generate a digital signature for a given input and private key.
+   *
+   * @param {string} input
+   * @param {string|JWK} privateKey
+   * @returns {string}
+   */
+  sign (input, privateKey) {
+    let {bitlength} = this
+    let signer = crypto.createSign(`RSA-SHA${bitlength}`)
+    signer.update(input)
+    return base64url.fromBase64(signer.sign(privateKey))
+  }
+
+  /**
+   * Verify
+   *
+   * @description
+   * Verify a digital signature for a given input and private key.
+   *
+   * @param {string} input
+   * @param {string} signature
+   * @param {string|JWK} publicKey
+   * @returns {Boolean}
+   */
+  verify (input, signature, publicKey) {
+    let {bitlength} = this
+    let base64sig = base64url.toBase64(signature)
+    let verifier = crypto.createVerify(`RSA-SHA${bitlength}`)
+    verifier.update(input)
+    return verifier.verify(publicKey, signature, 'base64')
+  }
+
+}
+
+/**
+ * Export
+ */
+module.exports = RSASSA_PKCS1_v1_5
---
+++ test/tokens/algs/RSASSA-PKCS1-v1_5-Spec.js
@@ -0,0 +1,86 @@
+'use strict'
+
+/**
+ * Test dependencies
+ */
+const cwd = process.cwd()
+const path = require('path')
+const chai = require('chai')
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+
+/**
+ * Assertions
+ */
+chai.use(sinonChai)
+chai.should()
+let expect = chai.expect
+
+/**
+ * Code under test
+ */
+const RS = require(path.join(cwd, 'src', 'tokens', 'algs', 'RSASSA-PKCS1-v1_5'))
+
+/**
+ * Tests
+ */
+describe('Digital Signature with RSASSA-PKCS1-v1_5', () => {
+
+  /**
+   * 3.3.  Digital Signature with RSASSA-PKCS1-v1_5
+   *
+   *   This section defines the use of the RSASSA-PKCS1-v1_5 digital
+   *   signature algorithm as defined in Section 8.2 of RFC 3447 [RFC3447]
+   *   (commonly known as PKCS #1), using SHA-2 [SHS] hash functions.
+   *
+   *   A key of size 2048 bits or larger MUST be used with these algorithms.
+   *
+   *   The RSASSA-PKCS1-v1_5 SHA-256 digital signature is generated as
+   *   follows: generate a digital signature of the JWS Signing Input using
+   *   RSASSA-PKCS1-v1_5-SIGN and the SHA-256 hash function with the desired
+   *   private key.  This is the JWS Signature value.
+   *
+   *   The following "alg" (algorithm) Header Parameter values are used to
+   *   indicate that the JWS Signature is a digital signature value computed
+   *   using the corresponding algorithm:
+   *
+   *          +-------------------+---------------------------------+
+   *          | "alg" Param Value | Digital Signature Algorithm     |
+   *          +-------------------+---------------------------------+
+   *          | RS256             | RSASSA-PKCS1-v1_5 using SHA-256 |
+   *          | RS384             | RSASSA-PKCS1-v1_5 using SHA-384 |
+   *          | RS512             | RSASSA-PKCS1-v1_5 using SHA-512 |
+   *          +-------------------+---------------------------------+
+   *
+   *   The RSASSA-PKCS1-v1_5 SHA-256 digital signature for a JWS is
+   *   validated as follows: submit the JWS Signing Input, the JWS
+   *   Signature, and the public key corresponding to the private key used
+   *   by the signer to the RSASSA-PKCS1-v1_5-VERIFY algorithm using SHA-256
+   *   as the hash function.
+   *
+   *   Signing and validation with the RSASSA-PKCS1-v1_5 SHA-384 and RSASSA-
+   *   PKCS1-v1_5 SHA-512 algorithms is performed identically to the
+   *   procedure for RSASSA-PKCS1-v1_5 SHA-256 -- just using the
+   *   corresponding hash algorithms instead of SHA-256.
+   *
+   *   An example using this algorithm is shown in Appendix A.2 of [JWS].
+   */
+  describe('constructor', () => {
+    it('should set bitlength')
+  })
+
+  describe('sign', () => {
+    it('should enforce minimum key size')
+    it('should return a base64url-encoded hash-based message authentication code')
+  })
+
+  describe('verify', () => {
+    it('should return true with valid input')
+    it('should return false with invalid input')
+  })
+
+  describe('assertSufficientKeySize', () => {
+    it('should not throw an error with a sufficient key length')
+    it('should throw an error with insufficient key length')
+  })
+})

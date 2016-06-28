9264b71d2c3d57638b2c269d740cf948ad6dd311
Christian Smith <smith@anvil.io>
2016-06-28 13:02:09 -0700

    feat(JWA): initial None class definition

---
+++ src/tokens/algs/None.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/**
+ * Dependencies
+ * @ignore
+ */
+
+/**
+ * "none" Algorithm
+ */
+class None {
+  sign () {
+    return ''
+  }
+}
+
+/**
+ * Export
+ */
+module.exports = None

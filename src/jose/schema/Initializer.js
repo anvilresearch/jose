796f4fe1e9a7aa81bbd2e259d34e86ae1bb8e16e
Christian Smith <smith@anvil.io>
2016-07-02 01:37:05 -0700

    feat(Initializer): initial class definition

---
+++ src/jose/schema/Initializer.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/**
+ * Dependencies
+ * @ignore
+ */
+
+/**
+ * Initializer
+ */
+class Initializer {
+
+  /**
+   * Compile
+   */
+  static compile () {}
+}
+
+/**
+ * Export
+ */
+module.exports = Initializer

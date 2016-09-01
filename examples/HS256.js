fb41285244b6cd16750970bb4fbced1c8cf8242d
Christian Smith <smith@anvil.io>
2016-09-01 10:17:22 -0700

    docs(examples): HS256 and RS256 signature examples

---
+++ examples/HS256.js
@@ -0,0 +1,40 @@
+const crypto = require('webcrypto')
+const JWA = require('../src/jose/JWAwc')
+
+let key
+
+crypto.subtle
+
+  // use webcrypto to generate a keypair
+  .generateKey(
+    {
+      name: 'HMAC',
+      hash: {
+        name: 'SHA-256'
+      }
+    },
+    true,
+    ['sign', 'verify']
+  )
+
+  // use key with JWA to create a signature
+  .then(result => {
+    key = result
+
+    return JWA.sign('HS256', key, 'header.payload')
+  })
+
+  // verify the signature
+  .then(signature => {
+    return Promise.all([
+      Promise.resolve(signature),
+      JWA.verify('HS256', key, signature, 'header.payload'),
+      JWA.verify('HS256', key, signature, 'wrong'),
+    ])
+  })
+
+  // look at the output
+  .then(console.log)
+
+  // look at the out
+  .catch(console.log)
---
+++ examples/RS256.js
@@ -0,0 +1,41 @@
+const crypto = require('webcrypto')
+const JWA = require('../src/jose/JWAwc')
+
+let privateKey, publicKey
+
+crypto.subtle
+
+  // use webcrypto to generate a keypair
+  .generateKey(
+    {
+      name: 'RSASSA-PKCS1-v1_5',
+      hash: {
+        name: 'SHA-256'
+      }
+    },
+    true,
+    ['sign', 'verify']
+  )
+
+  // use key with JWA to create a signature
+  .then(keypair => {
+    privateKey = keypair.privateKey
+    publicKey = keypair.publicKey
+
+    return JWA.sign('RS256', privateKey, 'header.payload')
+  })
+
+  // verify the signature
+  .then(signature => {
+    return Promise.all([
+      Promise.resolve(signature),
+      JWA.verify('RS256', publicKey, signature, 'header.payload'),
+      JWA.verify('RS256', publicKey, signature, 'wrong'),
+    ])
+  })
+
+  // look at the output
+  .then(console.log)
+
+  // look at the out
+  .catch(console.log)

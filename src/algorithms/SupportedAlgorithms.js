db3e3152fd43746e3e3172bdbbf8304cbebe3576
Christian Smith <smith@anvil.io>
2016-08-31 21:59:38 -0700

    feat(SupportedAlgorithms): initial class definition

---
+++ src/algorithms/SupportedAlgorithms.js
@@ -0,0 +1,79 @@
+/**
+ * Operations
+ */
+const operations = [
+  'sign',
+  'verify',
+  'encrypt',
+  'decrypt'
+]
+
+/**
+ * SupportedAlgorithms
+ */
+class SupportedAlgorithms {
+
+  /**
+   * constructor
+   */
+  constructor () {
+    operations.forEach(op => {
+      this[op] = {}
+    })
+  }
+
+  /**
+   * Supported Operations
+   */
+  static get operations () {
+    return operations
+  }
+
+  /**
+   * define
+   *
+   * @description
+   * Register Web Crypto API algorithm parameter for an algorithm
+   * and operation.
+   *
+   * @param {string} alg
+   * @param {string} op
+   * @param {Object} argument
+   */
+  define (alg, op, argument) {
+    let registeredAlgorithms = this[op]
+    registeredAlgorithms[alg] = argument
+  }
+
+  /**
+   * normalize
+   *
+   * @description
+   * Map JWA alg name to Web Crypto API algorithm parameter
+   *
+   * @param {string} op
+   * @param {Object} alg
+   *
+   * @returns {Object}
+   */
+  normalize (op, alg) {
+    let registeredAlgorithms = this[op]
+
+    if (!registeredAlgorithms) {
+      throw new SyntaxError() // what kind of error should this be?
+    }
+
+    let argument = registeredAlgorithms[alg]
+
+    if (!argument) {
+      throw new NotSupportedError()
+    }
+
+    return argument
+  }
+}
+
+/**
+ * Export
+ */
+module.exports = SupportedAlgorithms

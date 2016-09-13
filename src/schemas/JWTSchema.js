22347710fd69796c62ddf806833b87f5e37295f5
Christian Smith <smith@anvil.io>
2016-09-12 19:01:22 -0700

    refactor(JWTSchema): schema definition

--- src/schemas/JWTSchema.js
+++ src/schemas/JWTSchema.js
@@ -1,50 +1,18 @@
 /**
  * Dependencies
  */
+const JWESchema = require('./JWESchema')
+const JWSSchema = require('./JWSSchema')
 const {JSONSchema} = require('json-document')
 
 /**
  * JWT Schema
  */
 const JWTSchema = new JSONSchema({
-  type: 'object',
-  properties: {
-
-    /**
-     * JOSE Header
-     */
-    header: {
-      type: 'object',
-      properties: {
-        typ: { type: 'string' },
-        cty: { type: 'string' }
-      }
-    },
-
-    /**
-     * Registered Claim Names
-     *
-     * NOTES:
-     *
-     * 1. The `type` of payload is not explicitly set here as it is possible
-     *    for the payload to contain a value other than an object.
-     *
-     * 2. By default, additional claims are allowed. Additional claims can be
-     *    disallowed by extending schemas by setting the value of
-     *    `payload.additionalProperties` to `false`.
-     */
-    payload: {
-      properties: {
-        iss: { type: 'string', format: 'StringOrURI' },
-        sub: { type: 'string', format: 'StringOrURI' },
-        aud: { type: ['array', 'string'], format: 'StringOrURI' },
-        exp: { type: 'number', format: 'NumericDate' }, // uhm... how do we validate NumericDate?
-        nbf: { type: 'number', format: 'NumericDate' },
-        iat: { type: 'number', format: 'NumericDate' },
-        jti: { type: 'string' }
-      }
-    }
-  }
+  oneOf: [
+    JWESchema,
+    JWSSchema
+  ]
 })
 
 /**

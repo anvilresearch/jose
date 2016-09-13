b3c486e364288c3a51e5bf5db5bcddcd86c3626a
Christian Smith <smith@anvil.io>
2016-09-13 10:03:36 -0700

    refactor(JWTSchema): unified schema for JWS, JWE, and all serializations

--- src/schemas/JWTSchema.js
+++ src/schemas/JWTSchema.js
@@ -1,18 +1,102 @@
 /**
  * Dependencies
  */
-const JWESchema = require('./JWESchema')
-const JWSSchema = require('./JWSSchema')
+const Base64URLSchema = require('./Base64URLSchema')
+const JWTClaimsSetSchema = require('./JWTClaimsSetSchema')
+const JOSEHeaderSchema = require('./JOSEHeaderSchema')
 const {JSONSchema} = require('json-document')
 
 /**
- * JWT Schema
+ * JWTSchema
+ *
+ * @description
+ * This schema represents all the things a deserialized JWT can be, i.e.,
+ * either a JWS or JWE, and any serialization of them. Validation of well-
+ * formedness for a given serialization is accomplished at the time of
+ * encoding.
  */
 const JWTSchema = new JSONSchema({
-  oneOf: [
-    JWESchema,
-    JWSSchema
-  ]
+  type: 'object',
+  properties: {
+
+    /**
+     * header
+     */
+    header: JOSEHeaderSchema,
+
+    /**
+     * protected
+     */
+    protected: JOSEHeaderSchema,
+
+    /**
+     * unprotected
+     */
+    unprotected: JOSEHeaderSchema,
+
+    /**
+     * iv
+     */
+    iv: Base64URLSchema,
+
+    /**
+     * aad
+     */
+    aad: Base64URLSchema,
+
+    /**
+     * ciphertext
+     */
+    ciphertext: Base64URLSchema,
+
+    /**
+     * tag
+     */
+    tag: Base64URLSchema,
+
+    /**
+     * recipients
+     */
+    recipients: {
+      type: 'array',
+      items: [
+        {
+          type: 'object',
+          properties: {
+            header: JOSEHeaderSchema,
+            encrypted_key: Base64URLSchema
+          }
+        }
+      ]
+    },
+
+    /**
+     * payload
+     */
+    payload: JWTClaimsSetSchema,
+
+    /**
+     * signatures
+     */
+    signatures: {
+      type: 'array',
+      items: [
+        {
+          type: 'object',
+          properties: {
+            protected: JOSEHeaderSchema,
+            header: JOSEHeaderSchema,
+            signature: Base64URLSchema
+          }
+        }
+      ]
+    },
+
+    /**
+     * signature
+     */
+    signature: Base64URLSchema
+  }
 })
 
 /**

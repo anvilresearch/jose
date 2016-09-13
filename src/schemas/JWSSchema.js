a4265fab078d375f147dd52536ca47d85b76da9e
Christian Smith <smith@anvil.io>
2016-09-12 19:01:03 -0700

    refactor(JWSSchema): schema definition

--- src/schemas/JWSSchema.js
+++ src/schemas/JWSSchema.js
@@ -1,88 +1,20 @@
 /**
  * Dependencies
  */
-const JWTSchema = require('./JWTSchema')
+const JWSCompactSerializationSchema = require('./JWSCompactSerializationSchema')
+const JWSJSONSerializationSchema = require('./JWSJSONSerializationSchema')
+const JWSFlattenedSerializationSchema = require('./JWSFlattenedSerializationSchema')
+const {JSONSchema} = require('json-document')
 
 /**
  * JWS Schema
  */
-const JWSSchema = JWTSchema.extend({
-  type: 'object',
-  properties: {
-
-    /**
-     * JOSE Header
-     */
-    header: {
-      properties: {
-        alg: {
-          type: 'string',
-          format: 'StringOrURI',
-          enum: [
-            'HS256',
-            'HS384',
-            'HS512',
-            'RS256',
-            'RS384',
-            'RS512',
-            'ES256',
-            'ES384',
-            'ES512',
-            'PS256',
-            'PS384',
-            'PS512',
-            'none'
-          ]
-        },
-        jku: {
-          type: 'string',
-          format: 'url'
-        },
-        jwk: {
-          $ref: 'https://...#jwk'
-        },
-        kid: {
-          type: 'string'
-        },
-        x5u: {
-          type: 'string',
-          format: 'url'
-        },
-        x5c: {
-          type: 'array',
-          format: 'base64' // NOT base64url
-        },
-        x5t: {
-          type: 'string',
-          format: 'base64url' // NOT base64
-        },
-        //'x5t#256': {
-        //  type: 'string',
-        //  format: 'base64url'
-        //},
-
-        // TODO
-        typ: {
-          type: 'string',
-          format: 'MediaType'
-        },
-
-        // TODO
-        cty: {
-          type: 'string',
-          format: 'MediaType'
-        },
-
-        crit: {
-          type: 'array',
-          enum: [] // is this possible?
-        }
-      },
-      required: [
-        'alg'
-      ]
-    }
-  }
+const JWSSchema = new JSONSchema({
+  oneOf: [
+    JWSCompactSerializationSchema,
+    JWSJSONSerializationSchema,
+    JWSFlattenedSerializationSchema
+  ]
 })
 
 /**

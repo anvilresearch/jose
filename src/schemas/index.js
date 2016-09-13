9f9926c14b940edfa6e5bd9cee3ecface3274afa
Christian Smith <smith@anvil.io>
2016-09-13 10:06:58 -0700

    refactor(schemas): remove deprecated JWE/JWS schemas

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -11,11 +11,9 @@ module.exports = {
   JWECompactSerializationSchema: require('./JWECompactSerializationSchema'),
   JWEFlattenedSerializationSchema: require('./JWEFlattenedSerializationSchema'),
   JWEJSONSerializationSchema: require('./JWEJSONSerializationSchema'),
-  JWESchema: require('./JWESchema'),
   JWSCompactSerializationSchema: require('./JWSCompactSerializationSchema'),
   JWSFlattenedSerializationSchema: require('./JWSFlattenedSerializationSchema'),
   JWSJSONSerializationSchema: require('./JWSJSONSerializationSchema'),
-  JWSSchema: require('./JWSSchema'),
   JWTClaimsSetSchema: require('./JWTClaimsSetSchema'),
   JWTSchema: require('./JWTSchema'),
 }

521c2a0d0f9447bbd5ad767d120ddea8856e23fc
Christian Smith <smith@anvil.io>
2016-10-07 18:38:50 -0700

    wip(jose): update exported modules

--- src/index.js
+++ src/index.js
@@ -3,31 +3,18 @@
  */
 const JWA = require('./jose/JWA')
 const JWK = require('./jose/JWK')
-const KeyChain = require('./keys/KeyChain')
-const KeyPair = require('./keys/KeyPair')
-const PEM = require('./keys/PEM')
-const EncryptedJWK = require('./keys/EncryptedJWK')
-const RSAKeyPair = require('./keys/RSAKeyPair')
-const RSAPrivateKey = require('./keys/RSAPrivateKey')
-const RSAPublicKey = require('./keys/RSAPublicKey')
-const ECKeyPair = require('./keys/ECKeyPair')
-const ECPrivateKey = require('./keys/ECPrivateKey')
-const ECPublicKey = require('./keys/ECPublicKey')
+const JWKSet = require('./jose/JWKSet')
+const JWT = require('./jose/JWT')
+const JWS = require('./jose/JWS')
+
 
 /**
  * Export
  */
-module.exports = { 
+module.exports = {
   JWA,
   JWK,
-  KeyChain,
-  KeyPair,
-  PEM,
-  EncryptedJWK,
-  RSAKeyPair,
-  RSAPrivateKey,
-  RSAPublicKey,
-  ECKeyPair,
-  ECPrivateKey,
-  ECPublicKey
+  JWKSet,
+  JWT,
+  JWS
 }

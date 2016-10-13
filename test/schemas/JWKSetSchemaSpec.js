0af512cadfe44a94afa1ce6869f02800829e1012
Christian Smith <smith@anvil.io>
2016-10-12 18:21:55 -0700

    test(JWKSetSchema): stubs

---
+++ test/schemas/JWKSetSchemaSpec.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/**
+ * Test dependencies
+ */
+const chai = require('chai')
+
+/**
+ * Assertions
+ */
+chai.should()
+let expect = chai.expect
+
+/**
+ * Code under test
+ */
+const JWKSetSchema = require('../../src/schemas/JWKSetSchema')
+
+/**
+ * Tests
+ */
+describe('JWKSetSchema', () => {
+  it('should be an instance of JSONSchema')
+  it('should define type')
+  it('should define "keys" type')
+  it('should define "keys" items')
+})
+

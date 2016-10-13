0a0111bce90fdc6b0f117bccce42842e5d3ebcf7
Christian Smith <smith@anvil.io>
2016-10-12 18:22:39 -0700

    test(JWTClaimsSetSchema): stubs

---
+++ test/schemas/JWTClaimsSetSchemaSpec.js
@@ -0,0 +1,39 @@
+'use strict'
+
+/**
+ * Test dependencies
+ */
+const chai = require('chai')
+
+/**
+ * Assertions
+ */
+chai.should()
+let expect = chai.expect
+
+/**
+ * Code under test
+ */
+const JWTClaimsSetSchema = require('../../src/schemas/JWTClaimsSetSchema')
+
+/**
+ * Tests
+ */
+describe('JWTClaimsSetSchema', () => {
+  it('should be an instance of JSONSchema')
+  it('should define "iss" type')
+  it('should define "iss" format')
+  it('should define "sub" type')
+  it('should define "sub" format')
+  it('should define "aud" type')
+  it('should define "aud" format')
+  it('should define "aud" items format')
+  it('should define "exp" type')
+  it('should define "exp" format')
+  it('should define "nbf" type')
+  it('should define "nbf" format')
+  it('should define "iat" type')
+  it('should define "iat" format')
+  it('should define "jti" type')
+})
+

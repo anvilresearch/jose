78bd41cdc5484549bf8448970395f9747e679c0a
Christian Smith <smith@anvil.io>
2016-10-12 18:22:54 -0700

    test(JWTSchema): stubs

---
+++ test/schemas/JWTSchemaSpec.js
@@ -0,0 +1,55 @@
+'use strict'
+
+/**
+ * Test dependencies
+ */
+const chai = require('chai')
+
+/**
+ * Assertions
+ */
+chai.should()
+let expect = chai.expect
+
+/**
+ * Code under test
+ */
+const JOSEHeaderSchema = require('../../src/schemas/JOSEHeaderSchema')
+
+/**
+ * Tests
+ */
+describe('JOSEHeaderSchema', () => {
+  it('should be an instance of JSONSchema')
+  it('should define type')
+  it('should define "segments" type')
+  it('should define "header"')
+  it('should define "protected"')
+  it('should define "unprotected"')
+  it('should define "iv"')
+  it('should define "aad"')
+  it('should define "ciphertext"')
+  it('should define "tag"')
+  it('should define "recipients" type')
+  it('should define "recipients" items')
+  it('should define "recipients" items type')
+  it('should define "recipients" items "header"')
+  it('should define "recipients" items "encrypted_key"')
+  it('should define "payload"')
+  it('should define "signatures" type')
+  it('should define "signatures" items')
+  it('should define "signatures" items type')
+  it('should define "signatures" items "protected"')
+  it('should define "signatures" items "header"')
+  it('should define "signatures" items "signature"')
+  it('should define "signatures" items "key"')
+  it('should define "signatures" items "key" type')
+  it('should define "signature"')
+  it('should define "verified" type')
+  it('should define "verified" default')
+  it('should define "key" type')
+  it('should define "serialization" type')
+  it('should define "serialization" enum')
+  it('should define "serialization" default')
+})
+

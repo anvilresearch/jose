e68fc2abd60603175df34d63175670a7bd30f765
Christian Smith <smith@anvil.io>
2016-10-12 18:21:13 -0700

    test(JOSEHeaderSchema): stubs

---
+++ test/schemas/JOSEHeaderSchemaSpec.js
@@ -0,0 +1,48 @@
+'use strict'
+
+/**
+ * Test dependencies
+ */
+const chai = require('chai')
+
+/**
+ * Assertions
+ */
+chai.should()
+let expect = chai.expect
+
+/**
+ * Code under test
+ */
+const JOSEHeaderSchema = require('../../src/schemas/JOSEHeaderSchema')
+
+/**
+ * Tests
+ */
+describe('JOSEHeaderSchema', () => {
+  it('should be an instance of JSONSchema')
+  it('should define type')
+  it('should define "typ" type')
+  it('should define "cty" type')
+  it('should define "cty" enum')
+  it('should define "alg" type')
+  it('should define "alg" format')
+  it('should define "jku" type')
+  it('should define "jku" format')
+  it('should define "jwk" type')
+  it('should define "kid" type')
+  it('should define "x5u" type')
+  it('should define "x5c" type')
+  it('should define "x5c" format')
+  it('should define "x5t" type')
+  it('should define "x5t" format')
+  it('should define "x5t#S256" type')
+  it('should define "x5t#S256" format')
+  it('should define "crit" type')
+  it('should define "crit" items')
+  it('should define "crit" minItems')
+  it('should define "enc" type')
+  it('should define "enc" format')
+  it('should define "zip" type')
+})
+

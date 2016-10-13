4bd45febd893ea414f448936aad1b4c4ba15affe
Christian Smith <smith@anvil.io>
2016-10-12 18:20:44 -0700

    test(Base64URLSchema): stubs

---
+++ test/schemas/Base64URLSchemaSpec.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/**
+ * Test dependencies
+ */
+const chai = require('chai')
+
+/**
+ * Assertions
+ */
+chai.should()
+let expect = chai.expect
+
+/**
+ * Code under test
+ */
+const Base64URLSchema = require('../../src/schemas/Base64URLSchema')
+
+/**
+ * Tests
+ */
+describe('Base64URLSchema', () => {
+  it('should be an instance of JSONSchema')
+  it('should define type')
+  it('should define format')
+})
+

ea0119d9f3607905d21d991612ead2e90587a473
Christian Smith <smith@anvil.io>
2016-10-17 16:16:02 -0700

    test(SupportedAlgorithms): constructor, operations, define, and normalize methods

--- test/algorithms/SupportedAlgorithmsSpec.js
+++ test/algorithms/SupportedAlgorithmsSpec.js
@@ -21,20 +21,52 @@ const SupportedAlgorithms = require('../../src/algorithms/SupportedAlgorithms')
  */
 describe('SupportedAlgorithms', () => {
   describe('constructor', () => {
-    it('should initialize registers')
+    it('should initialize registers', () => {
+      let operations = SupportedAlgorithms.operations
+      let supportedAlgorithms = new SupportedAlgorithms()
+      operations.forEach(op => {
+        supportedAlgorithms[op].should.eql({})
+      })
+    })
   })
 
   describe('static get operations', () => {
-    it('should be an array of operations')
+    it('should be an array of operations', () => {
+      SupportedAlgorithms.operations.should.eql([
+        'sign',
+        'verify',
+        'encrypt',
+        'decrypt',
+        'importKey'
+      ])
+    })
   })
 
   describe('define', () => {
-    it('should register an algorithm for an operation')
+    it('should register an algorithm for an operation', () => {
+      let supportedAlgorithms = new SupportedAlgorithms()
+      let argument = {}
+      supportedAlgorithms.define('RS256', 'verify', argument)
+      supportedAlgorithms.verify['RS256'].should.equal(argument)
+    })
   })
 
   describe('normalize', () => {
-    it('should return a registered algorithm for an operation by name')
-    it('should throw NotSupportedError for unknown algorithms')
+    it('should return a registered algorithm for an operation by name', () => {
+      let supportedAlgorithms = new SupportedAlgorithms()
+      let argument = {}
+      supportedAlgorithms.define('RS256', 'verify', argument)
+      supportedAlgorithms.normalize('verify', 'RS256').should.equal(argument)
+    })
+
+    it('should throw NotSupportedError for unknown algorithms', () => {
+      let supportedAlgorithms = new SupportedAlgorithms()
+      let argument = {}
+
+      expect(() => {
+        supportedAlgorithms.normalize('verify', 'RS256').should.equal(argument)
+      }).to.throw('RS256 is not a supported algorithm')
+    })
   })
 })
 

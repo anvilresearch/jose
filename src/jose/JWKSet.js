ae3b9c4c1cc08e6b352509483e9f4018c0f9e734
Dmitri Zagidulin <dzagidulin@gmail.com>
2017-03-27 12:45:54 -0400

    fix(JWKSet): Fix promise reject on validation errors
    - On validation errors in `importKeys()`, adds a missing `return` statement to the `Promise.reject()`
    - Rejects with an `Error` instead of a validation result object
    - Adds a failing test for upstream issue https://github.com/anvilresearch/json-document/issues/9
    - Handles cases where the `jwks.keys` property is empty/missing (was previously giving opaque
      `TypeError: Cannot read property 'map' of undefined` errors)

--- src/jose/JWKSet.js
+++ src/jose/JWKSet.js
@@ -33,9 +33,6 @@ class JWKSet extends JSONDocument {
       .then(() => {
         let validation = this.schema.validate(jwks)
 
-        // TODO: fix
-        validation.valid = true
-
         if (!validation.valid) {
           throw new Error('Invalid JWKSet: ' +
             JSON.stringify(validation, null, 2))

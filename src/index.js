7206ebf590907555aaaeeeff6b19955b85133c95
Greg Linklater <greglink49@gmail.com>
2017-04-25 21:57:44 +0200

    refactor(JWS): remove JWS references and tests

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const JWK = require('./jose/JWK')
 const JWKSet = require('./jose/JWKSet')
 const JWT = require('./jose/JWT')
 const JWD = require('./jose/JWD')
-const JWS = require('./jose/JWS')
 const Base64URLSchema = require('./schemas/Base64URLSchema')
 const JOSEHeaderSchema = require('./schemas/JOSEHeaderSchema')
 const JWKSchema = require('./schemas/JWKSchema')
@@ -24,7 +23,6 @@ module.exports = {
   JWKSet,
   JWT,
   JWD,
-  JWS,
   Base64URLSchema,
   JOSEHeaderSchema,
   JWKSchema,
